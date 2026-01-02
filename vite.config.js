36df7a0b5fc54428a6a006d93450f394bacb3d0c
Technofancy Learner <Vkhatri137@gmail.com>
2026-01-02 18:47:57 +0545

    base-repoName

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', //jnanalaya.edu.np
+  base: '/jnanalaya.edu.np', //jnanalaya.edu.np (reponame)
   plugins: [react()],
 })
