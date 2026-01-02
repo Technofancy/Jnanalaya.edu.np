84585e9bbc93eb16726cb3c8055c2cf613c19847
Technofancy Learner <Vkhatri137@gmail.com>
2026-01-02 19:03:51 +0545

    gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jnanalaya.edu.np', //jnanalaya.edu.np (reponame)
+  base: "/", //jnanalaya.edu.np (reponame)
   plugins: [react()],
-})
+});
