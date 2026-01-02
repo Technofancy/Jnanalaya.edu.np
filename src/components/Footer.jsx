1a9f0605064d9267b105bfde2dd19fb6582491f6
Technofancy Learner <Vkhatri137@gmail.com>
2026-01-02 19:58:28 +0545

    developer on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Footer = ({ t }) => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
-      <p>{t.copy}</p>
+      {/* Developed by Technofancy */}
+      <div className="flex items-center justify-center gap-2">
+        <p>
+          <a
+            href="https://bhuwankhatri.com.np/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-500"
+          >
+            Developed by Technofancy
+          </a>
+        </p>
+        <p>|</p>
+        <p>{t.copy}</p>
+      </div>
     </footer>
   );
 };
